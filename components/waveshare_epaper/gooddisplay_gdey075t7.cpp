ee9652d93b4e5f0fcb40680d5c9a50c8d49f052c
Brandon Chen <parkghost@gmail.com>
2025-03-24 04:29:26 +0800

    refactor: simplify display initialization and buffer handling in GDEY075T7

--- components/waveshare_epaper/gooddisplay_gdey075t7.cpp
+++ components/waveshare_epaper/gooddisplay_gdey075t7.cpp
@@ -11,8 +11,6 @@ const char *const GDEY075T7::TAG = "gdey075t7";
 
 int GDEY075T7::get_width_internal() { return WIDTH; }
 
-int GDEY075T7::get_width_controller() { return WIDTH; }
-
 int GDEY075T7::get_height_internal() { return HEIGHT; }
 
 uint32_t GDEY075T7::idle_timeout_() { return IDLE_TIMEOUT; }
@@ -29,22 +27,105 @@ void GDEY075T7::full_refresh() {
 void GDEY075T7::initialize() {}
 
 void GDEY075T7::display() {
-  RefreshMode mode = this->at_update_ == 0 ? FAST_REFRESH : PARTIAL_REFRESH;
+  RefreshMode mode = this->at_update_ == 0 ? FULL_REFRESH : PARTIAL_REFRESH;
   this->at_update_ = (this->at_update_ + 1) % this->full_update_every_;
 
-  this->init_display_(mode);
+  this->init_display_();
 
+  this->write_buffer_(mode);
+
+  this->status_clear_warning();
+
+  this->deep_sleep();
+}
+
+void GDEY075T7::init_display_() {
+  reset_();
+
+  this->wait_until_idle_();
+
+  this->command(0x01);  // POWER SETTING
+  this->data(0x07);
+  this->data(0x07);  // VGH=20V,VGL=-20V
+  this->data(0x3f);  // VDH=15V
+  this->data(0x3f);  // VDL=-15V
+
+  // Enhanced display drive(Add 0x06 command)
+  this->command(0x06);  // Booster Soft Start
+  this->data(0x17);
+  this->data(0x17);
+  this->data(0x28);
+  this->data(0x17);
+
+  this->command(0x04);  // POWER ON
+  delay(100);
+  this->wait_until_idle_();  // waiting for the electronic paper IC to
+                             // release the idle signal
+
+  this->command(0x00);  // PANNEL SETTING
+  this->data(0x1F);     // KW-3f   KWR-2F BWROTP 0f BWOTP 1f
+
+  this->command(0x61);  // tres
+  this->data(0x03);     // source 800
+  this->data(0x20);
+  this->data(0x01);  // gate 480
+  this->data(0xE0);
+
+  this->command(0x15);
+  this->data(0x00);
+
+  this->command(0x50);  // VCOM AND DATA INTERVAL SETTING
+  this->data(0x10);
+  this->data(0x07);
+
+  this->command(0x60);  // TCON SETTING
+  this->data(0x22);
+}
+
+void GDEY075T7::write_buffer_(RefreshMode mode) {
   switch (mode) {
     case FULL_REFRESH:
+      this->clear_();
+      for (uint32_t i = 0; i < this->get_buffer_length_(); i++) {
+        oldData_[i] = 0xff;
+      }
+      this->write_buffer_(PARTIAL_REFRESH);
+      return;
+
     case FAST_REFRESH:
+      this->command(0x00);  // PANNEL SETTING
+      this->data(0x1F);     // KW-3f   KWR-2F BWROTP 0f BWOTP 1f
+
+      this->command(0x50);  // VCOM AND DATA INTERVAL SETTING
+      this->data(0x10);
+      this->data(0x07);
+
+      this->command(0x04);  // POWER ON
+      delay(100);
+      this->wait_until_idle_();  // waiting for the electronic paper IC to
+                                 // release the idle signal
+
+      // Enhanced display drive(Add 0x06 command)
+      this->command(0x06);  // Booster Soft Start
+      this->data(0x27);
+      this->data(0x27);
+      this->data(0x18);
+      this->data(0x17);
+
+      this->command(0xE0);
+      this->data(0x02);
+      this->command(0xE5);
+      this->data(0x5A);
+
       // Write old Data
       this->command(0x10);
       this->start_data_();
-      for (uint32_t i = 0; i < this->get_buffer_length_(); i++) this->write_byte(0x00);
+      for (uint32_t i = 0; i < this->get_buffer_length_(); i++)
+        this->write_byte(0x00);
       this->end_data_();
 
       // Write new Data
-      this->command(0x13);
+      this->command(0x13);  // writes New data to SRAM.
       this->start_data_();
       for (uint32_t i = 0; i < this->get_buffer_length_(); i++) {
         this->write_byte(~this->buffer_[i]);
@@ -54,6 +135,18 @@ void GDEY075T7::display() {
       break;
 
     case PARTIAL_REFRESH:
+      this->command(0x00);  // PANNEL SETTING
+      this->data(0x1F);     // KW-3f   KWR-2F BWROTP 0f BWOTP 1f
+
+      this->command(0x04);  // POWER ON
+      delay(100);
+      this->wait_until_idle_();  // waiting for the electronic paper IC to
+                                 // release the idle signal
+      this->command(0xE0);
+      this->data(0x02);
+      this->command(0xE5);
+      this->data(0x6E);
+
       this->command(0x50);
       this->data(0xA9);
       this->data(0x07);
@@ -81,6 +174,8 @@ void GDEY075T7::display() {
       this->end_data_();
       for (uint32_t i = 0; i < this->get_buffer_length_(); i++)
         oldData_[i] = this->buffer_[i];
+
+      this->command(0x92);
       break;
 
     default:
@@ -96,109 +191,30 @@ void GDEY075T7::display() {
     this->status_set_warning();
     return;
   }
-
-  if (mode == PARTIAL_REFRESH) this->command(0x92);
-
-  this->status_clear_warning();
-
-  this->deep_sleep();
 }
 
-// Fast update 1 initialization
-void GDEY075T7::init_display_(RefreshMode mode) {
-  if (!initial_) {
-    reset_();
-    initial_ = true;
-  }
+void GDEY075T7::clear_() {
+  // Write old Data
+  this->command(0x10);
+  this->start_data_();
+  for (uint32_t i = 0; i < this->get_buffer_length_(); i++)
+    this->write_byte(0x00);
+  this->end_data_();
 
-  if (hibernating_) reset_();
+  // Write new Data
+  this->command(0x13);
+  this->start_data_();
+  for (uint32_t i = 0; i < this->get_buffer_length_(); i++)
+    this->write_byte(0x00);
+  this->end_data_();
 
-  this->wait_until_idle_();
-
-  switch (mode) {
-    case FULL_REFRESH:
-      this->command(0x01);  // POWER SETTING
-      this->data(0x07);
-      this->data(0x07);  // VGH=20V,VGL=-20V
-      this->data(0x3f);  // VDH=15V
-      this->data(0x3f);  // VDL=-15V
-
-      // Enhanced display drive(Add 0x06 command)
-      this->command(0x06);  // Booster Soft Start
-      this->data(0x17);
-      this->data(0x17);
-      this->data(0x28);
-      this->data(0x17);
-
-      this->command(0x04);  // POWER ON
-      delay(100);
-      this->wait_until_idle_();  // waiting for the electronic paper IC to
-                                 // release the idle signal
-
-      this->command(0x00);  // PANNEL SETTING
-      this->data(0x1F);     // KW-3f   KWR-2F BWROTP 0f BWOTP 1f
-
-      this->command(0x61);  // tres
-      this->data(0x03);     // source 800
-      this->data(0x20);
-      this->data(0x01);  // gate 480
-      this->data(0xE0);
-
-      this->command(0x15);
-      this->data(0x00);
-
-      this->command(0x50);  // VCOM AND DATA INTERVAL SETTING
-      this->data(0x10);
-      this->data(0x07);
-
-      this->command(0x60);  // TCON SETTING
-      this->data(0x22);
-      break;
-
-    case FAST_REFRESH:
-      this->command(0x00);  // PANNEL SETTING
-      this->data(0x1F);     // KW-3f   KWR-2F BWROTP 0f BWOTP 1f
-
-      this->command(0x50);  // VCOM AND DATA INTERVAL SETTING
-      this->data(0x10);
-      this->data(0x07);
-
-      this->command(0x04);  // POWER ON
-      delay(100);
-      this->wait_until_idle_();  // waiting for the electronic paper IC to
-                                 // release the idle signal
-
-      // Enhanced display drive(Add 0x06 command)
-      this->command(0x06);  // Booster Soft Start
-      this->data(0x27);
-      this->data(0x27);
-      this->data(0x18);
-      this->data(0x17);
-
-      this->command(0xE0);
-      this->data(0x02);
-      this->command(0xE5);
-      this->data(0x5A);
-      break;
-
-    case PARTIAL_REFRESH:
-      this->command(0x00);  // PANNEL SETTING
-      this->data(0x1F);     // KW-3f   KWR-2F BWROTP 0f BWOTP 1f
-
-      this->command(0x04);  // POWER ON
-      delay(100);
-      this->wait_until_idle_();  // waiting for the electronic paper IC to
-                                 // release the idle signal
-
-      this->command(0xE0);
-      this->data(0x02);
-      this->command(0xE5);
-      this->data(0x6E);
-      break;
-
-    default:
-      ESP_LOGE(TAG, "unsupported refresh mode, mode:%d", mode);
-      return;
+  this->command(0x12);  // DISPLAY update
+  delay(1);             //!!!The delay here is necessary, 200uS at least!!!
+  if (!this->wait_until_idle_())  // waiting for the electronic paper IC to
+                                  // release the idle signal
+  {
+    this->status_set_warning();
+    return;
   }
 }
 
@@ -209,13 +225,9 @@ void GDEY075T7::reset_() {
     this->reset_pin_->digital_write(true);
     delay(10);
   }
-
-  hibernating_ = false;
 }
 
 void GDEY075T7::deep_sleep() {
-  if (hibernating_) return;
-
   this->command(0x50);  // VCOM AND DATA INTERVAL SETTING
   this->data(0xf7);     // WBmode:VBDF 17|D7 VBDW 97 VBDB 57    WBRmode:VBDF F7
                         // VBDW 77 VBDB 37  VBDR B7
@@ -227,7 +239,6 @@ void GDEY075T7::deep_sleep() {
 
   this->command(0x07);  // deep sleep
   this->data(0xA5);
-  hibernating_ = true;
 }
 
 void GDEY075T7::dump_config() {
--- components/waveshare_epaper/gooddisplay_gdey075t7.h
+++ components/waveshare_epaper/gooddisplay_gdey075t7.h
@@ -28,23 +28,23 @@ class GDEY075T7 : public WaveshareEPaper {
  protected:
   int get_width_internal() override;
 
-  int get_width_controller() override;
-
   int get_height_internal() override;
 
   uint32_t idle_timeout_() override;
 
   bool is_busy_pin_inverted_() override { return true; }
 
-  void init_display_(RefreshMode mode);
+  void init_display_();
+
+  void write_buffer_(RefreshMode mode);
+
+  void clear_();
 
   void reset_();
 
   uint8_t oldData_[WIDTH * HEIGHT / 8];
 
   uint32_t at_update_{0}, full_update_every_{30};
-
-  bool initial_{false}, hibernating_{false};
 };
 
 }  // namespace waveshare_epaper
